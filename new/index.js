67c00a849548daf2b0b982bef26af0424f1dc089
rickthor7 <96973185+rickthor7@users.noreply.github.com>
2025-05-07 19:47:02 +0700

    Update index.js

--- new/index.js
+++ new/index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index'));
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.get('/test', (req, res) => {
